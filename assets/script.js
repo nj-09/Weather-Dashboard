2a80d0ce05127cfcd0bce537f561538dbd1ee30a
Nishit <nishitjoshi.ichs@gmail.com>
2023-03-12 03:22:30 +0000

    latest changes

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 // Global variables
-
-
+var searchInput = document.querySelector("#search-input")
+var searchButton = document.querySelector("#search-button")
+var historyDiv = document.querySelector("#history")
+var historyButton = document.querySelector("#history-button")
+var apiKey = "3f75236b7925362e7c2aa1e412e899f6"
 
 // API Key from openweather app
 // 3f75236b7925362e7c2aa1e412e899f6 
+
