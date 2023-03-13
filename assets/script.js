3ce0852889f5dfb332afbda3056723699c572718
Nishit <nishitjoshi.ichs@gmail.com>
2023-03-13 02:47:47 +0000

    fetchWeather function and their components

--- assets/script.js
+++ assets/script.js
@@ -28,46 +28,58 @@ var weatherApiRootUrl = 'https://api.openweathermap.org';
 //-------------------------------
 
 function fetchWeather(location) {
-    var lat = 51.500153;
-    var lon = -0.1262362;
-    // var city = London;
+    var lat = 51.509865;
+    var lon = -0.118092;
+  //   var city = London;
     var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-
+  
     fetch(apiUrl)
-    .then(function (res) {
+      .then(function (res) {
         return res.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(data.list);
-        console.log(data.list[0]);
-        console.log(data.list[0].dt);
-        console.log(data.list[0].wind.speed)
-        console.log(data.list[0].main.temp)
-        var windSpeed = data.list[0].wind.speed;
-        var windEl = document.createElement('p');
-        console.log(windEl);
-        var mainTemp = data.list[0].main.temp;
-        var tempEl = document.createElement('p');
-        console.log(tempEl)
-        
-
-    })
-     // renderItems(city, data);
-
-
-    .catch(function (err) {
+      })
+      .then(function (data) {
+         console.log(data);
+         console.log(data.list);
+         console.log(data.list[0]);
+         console.log(data.list[0].dt);
+         console.log(data.list[0].wind.speed);
+         var weather = data.list[0]
+         var tempF = weather.main.temp;
+         var windMph = weather.wind.speed;
+         var humidity = weather.main.humidity;
+         var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+         var iconDescription = weather.weather[0].description || weather[0].main;
+         console.log(tempF);
+         console.log(windMph);
+         console.log(humidity);
+         console.log(iconUrl);
+         console.log(iconDescription);
+       
+         var card = document.createElement('div');
+         var cardBody = document.createElement('div');
+         var heading = document.createElement('h2');
+         var weatherIcon = document.createElement('img');
+         var tempEl = document.createElement('p');
+         var windEl = document.createElement('p');
+         var humidityEl = document.createElement('p');
+  
+         weatherIcon.setAttribute('src', iconUrl);
+         weatherIcon.setAttribute('alt', iconDescription);
+         weatherIcon.setAttribute('class', 'weather-img');
+         console.log(weatherIcon)
+        // renderItems(city, data);
+      })
+      .catch(function (err) {
         console.error(err);
-    });
-}
-
+      });
+  }
+  
 fetchWeather();
+
+
+
 
   // For loop to register 5 days worth of datapoints until the 5th day
 for (let i = 1; i < 6; i++) {
     const element = array[i];
 }
-
-
-
-
