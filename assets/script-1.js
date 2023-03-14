ff4409585bfe74b64061de4b5edbc76ff3b05ab1
Nishit <nishitjoshi.ichs@gmail.com>
2023-03-14 18:14:50 +0000

    updating new JS file V1 with localStorage

---
+++ assets/script-1.js
@@ -0,0 +1,196 @@
+// Global variables
+var searchInput = document.querySelector("#search-input")
+var searchButton = document.querySelector("#search-button")
+var historyDiv = document.querySelector("#history")  // we already have a variable, so we can append here
+var historyButton = document.querySelector("#history-button")
+var apiKey = "3f75236b7925362e7c2aa1e412e899f6"
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+
+// API Key from openweather app
+// 3f75236b7925362e7c2aa1e412e899f6 
+
+// ----- Psuedocode ----- //
+// 1. make a function that is connected to a eventlistener that when the search is made it activates the fetch call.
+//2. hard code all the variables for the api
+//3. console.log the data that you receive from the fetch call
+//4. then console.log the data inside the fetch call to get the temp, wind speed, date.....etc.
+//5. do this for only the single day first before you go into the 5 day.
+
+// ------------------------------
+
+//1. console.log each data point
+//2. put each data point into a variable
+//3. create an element inside of your javascript for each of the variables
+//4. append those elements to the corresponding div on the html
+
+//-------------------------------
+
+function fetchWeather(location) {
+    var lat = 51.509865;
+    var lon = -0.118092;
+  //   var city = London;
+  //var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=3f75236b7925362e7c2aa1e412e899f6&units=imperial`;
+    
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+         console.log(data);
+   
+         var weather = data.weather[0].description || data.weather[0].main;
+         var tempF = data.main.temp;
+         var windMph = data.wind.speed;
+         var humidity = data.main.humidity;
+         var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+         var iconDescription = data.weather[0].description || data.weather[0].main;
+         var card = document.createElement('div');
+         var cardBody = document.createElement('div');
+         var heading = document.createElement('h2');
+         var weatherIcon = document.createElement('img');
+         var tempEl = document.createElement('p');
+         var windEl = document.createElement('p');
+         var humidityEl = document.createElement('p');
+        heading.textContent = location 
+        tempEl.textContent = "Temp:" + tempF;
+        windEl.textContent = "Wind Speed:"+windMph;
+        humidityEl.textContent = "Humidity: "+humidity;
+        weatherIcon.setAttribute('src', iconUrl);
+        weatherIcon.setAttribute('alt', iconDescription);
+        weatherIcon.setAttribute('class', 'weather-img');
+        cardBody.append(heading)
+        cardBody.append(tempEl)
+        cardBody.append(weatherIcon)
+        cardBody.append(windEl)
+        cardBody.append(humidityEl)
+
+        card.append(cardBody)
+       
+         document.getElementById("today").append(card);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+
+  function fetch5dayWeather(location) {
+    var lat = 51.509865;
+    var lon = -0.118092;
+  //   var city = London;
+  //var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=3f75236b7925362e7c2aa1e412e899f6&units=imperial`;
+    
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+         console.log(data);
+         var sectionEl = document.createElement("div")
+        for(let i =0;i<data.list.length;i=i+8){
+         var weather = data.list[i].weather[0].description || data.list[i].weather[0].main;
+         var tempF = data.list[i].main.temp;
+         var windMph = data.list[i].wind.speed;
+         var humidity = data.list[i].main.humidity;
+         var iconUrl = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
+         var iconDescription = data.list[i].weather[0].description || data.list[i].weather[0].main;
+         var card = document.createElement('div');
+         var cardBody = document.createElement('div');
+         var heading = document.createElement('h2');
+         var weatherIcon = document.createElement('img');
+         var tempEl = document.createElement('p');
+         var windEl = document.createElement('p');
+         var humidityEl = document.createElement('p'); // similar item to reference
+        heading.textContent = location 
+        tempEl.textContent = "Temp:" + tempF;
+        windEl.textContent = "Wind Speed:"+windMph;
+        humidityEl.textContent = "Humidity: "+humidity;
+        weatherIcon.setAttribute('src', iconUrl);
+        weatherIcon.setAttribute('alt', iconDescription);
+        weatherIcon.setAttribute('class', 'weather-img');
+        cardBody.append(heading)
+        cardBody.append(tempEl)
+        cardBody.append(weatherIcon)
+        cardBody.append(windEl)
+        cardBody.append(humidityEl)
+
+        card.append(cardBody)
+        sectionEl.append(card)
+        }
+         document.getElementById("forecast").append(sectionEl);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+
+
+searchButton.addEventListener("click",function(event){
+   event.preventDefault()
+   var location = searchInput.value
+   fetchWeather(location)
+   fetch5dayWeather(location)
+
+   // Pull previously saved cities OR if no saved cities create empty array
+
+   // you'll need to add in a contingency for when you have saved something. Otherwise if you just start it as an amty array, you'll always overwrite it and have an empty array. For example:
+
+   //var searchHistory = []; // it will always be empty if you do it this way, but if you've already search for a city, you want to check local storage and pull that in, so the alternative is:
+  var searchHistory = JSON.parse(localStorage.getItem('cities')) || []
+
+   // Save new location to saved city array
+
+   // push our new city into this array searchHistory
+   searchHistory.push(location)
+
+   // store updated saved city array in local storage 
+  localStorage.setItem("cities", JSON.stringify(searchHistory));
+console.log(JSON.parse(localStorage.getItem("cities")));
+
+// to call the function when a new city has been added to the search history.
+populateButtons ()
+
+})
+
+
+function populateButtons () {
+
+  //clear out old code
+  historyDiv.innerHTML = "";
+
+  // we need to grab the stored cities from local storage and store them in a variable
+  var storedButtons = JSON.parse(localStorage.getItem("cities"));
+
+  for (let i = 0; i < storedButtons.length; i++) {
+
+  // create button elelements
+// we dynamically create the button element
+var buttonEl = document.createElement('button');
+  // add the text content
+buttonEl.textContent = storedButtons[i] // ?
+  //add event listener to key off function
+
+    buttonEl.addEventListener('click', function(){
+      //if the button is clicked, we want those functions to start with the city name populating
+      fetchWeather(storedButtons[i])
+      fetch5dayWeather(storedButtons[i]) 
+
+    })
+
+  //append button to the page
+  historyDiv.append(buttonEl) 
+
+
+
+}
+
+}
+
+
+
+
+// To call the function on page load
+populateButtons ()
+
--- assets/script.js
+++ assets/script.js
@@ -81,6 +81,97 @@ fetchWeather();
 
 
   // For loop to register 5 days worth of datapoints until the 5th day
-for (let i = 1; i < 6; i++) {
-    const element = array[i];
-}
+// for (let i = 1; i < 6; i++) {
+    // const element = array[i];
+// }
+
+// Global variables
+var searchInput = document.querySelector("#search-input")
+var searchButton = document.querySelector("#search-button")
+var historyDiv = document.querySelector("#history")
+var historyButton = document.querySelector("#history-button")
+var apiKey = "3f75236b7925362e7c2aa1e412e899f6"
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+
+
+
+// API Key from openweather app
+// 3f75236b7925362e7c2aa1e412e899f6 
+
+// ----- Psuedocode ----- //
+// 1. make a function that is connected to a eventlistener that when the search is made it activates the fetch call.
+//2. hard code all the variables for the api
+//3. console.log the data that you receive from the fetch call
+//4. then console.log the data inside the fetch call to get the temp, wind speed, date.....etc.
+//5. do this for only the single day first before you go into the 5 day.
+
+// ------------------------------
+
+//1. console.log each data point
+//2. put each data point into a variable
+//3. create an element inside of your javascript for each of the variables
+//4. append those elements to the corresponding div on the html
+
+//-------------------------------
+
+function fetchWeather(location) {
+    var lat = 51.509865;
+    var lon = -0.118092;
+  //   var city = London;
+  //var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=3f75236b7925362e7c2aa1e412e899f6&units=imperial`;
+    
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+         console.log(data);
+   
+         var weather = data.weather[0].description || data.weather[0].main;
+         var tempF = data.main.temp;
+         var windMph = data.wind.speed;
+         var humidity = data.main.humidity;
+         var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+         var iconDescription = data.weather[0].description || data.weather[0].main;
+         var card = document.createElement('div');
+         var cardBody = document.createElement('div');
+         var heading = document.createElement('h2');
+         var weatherIcon = document.createElement('img');
+         var tempEl = document.createElement('p');
+         var windEl = document.createElement('p');
+         var humidityEl = document.createElement('p');
+        heading.textContent = location 
+        tempEl.textContent = "Temp:" + tempF;
+        windEl.textContent = "Wind Speed:"+windMph;
+        humidityEl.textContent = "Humidity: "+humidity;
+        weatherIcon.setAttribute('src', iconUrl);
+        weatherIcon.setAttribute('alt', iconDescription);
+        weatherIcon.setAttribute('class', 'weather-img');
+        cardBody.append(heading)
+        cardBody.append(tempEl)
+        cardBody.append(weatherIcon)
+        cardBody.append(windEl)
+        cardBody.append(humidityEl)
+
+        card.append(cardBody)
+       
+         document.getElementById("today").append(card);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
+  
+
+
+  // For loop to register 5 days worth of datapoints until the 5th day
+
+searchButton.addEventListener("click",function(event){
+   event.preventDefault()
+   var location = searchInput.value
+   fetchWeather(location)
+})
+
+
+// for localStorage, do we need to create a new JS file or can we do it here?
